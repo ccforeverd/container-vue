b20478c9757e333c4dafba20456b602b1ecf0160
ccforeverd <zh1045456074@163.com>
2020-11-19 16:18:05 +0800

    feat: add api/request/get

--- server/requests/get.ts
+++ server/requests/get.ts
@@ -1,71 +1,65 @@
+import vm from 'vm'
+// import URL from 'url'
+import { JSDOM } from 'jsdom'
 import { Service } from 'typedi'
 import Axios from 'axios'
 import Cheerio from 'cheerio'
-import { JsonController, Get, QueryParams } from 'routing-controllers'
-
-interface PageQuery {
-  url: string
-}
+import { JsonController, Get, QueryParam } from 'routing-controllers'
 
 async function getGlobalVars (url: string) {
-  const vm = require('vm')
   const { data: jsText } = await Axios.get(url)
   const script = new vm.Script(jsText)
-  const context = {}
-  script.runInNewContext(context)
-  console.log(context)
+  const { window: context } = new JSDOM('', {
+    url,
+    referrer: url,
+    contentType: 'text/html',
+    includeNodeLocations: true,
+    storageQuota: 10000000
+  })
+  const originKeys = Object.keys(context)
+  try {
+    script.runInNewContext(context)
+    await new Promise(resolve => setTimeout(resolve, 300))
+  } catch (e) {
+    return ['vm error', e]
+  }
+  const newKeys = Object.keys(context)
+  const newContext = newKeys
+    .filter(key => !originKeys.includes(key))
+    .reduce((result, key) => {
+      result[key] = context[key]
+      return result
+    }, {})
+  // TODO 销毁vm
+  return Object.keys(newContext)
+    .map(key => [key, newContext[key].version])
 }
-
-getGlobalVars('http://edu-fe.ks3-cn-beijing.ksyun.com/libs/vue-router.3.0.2.min.js')
-
-// async function getGlobalVars (url) {
-//   const { data: jsText } = await Axios.get(url)
-//   const fail = (msg, other = {}) => ({
-//     result: [],
-//     // jsText,
-//     msg,
-//     ...other
-//   })
-//   // !function(e,t)
-//   // 获取第一个变量
-//   const match = jsText.match(/!function\((\w),(\w)/)
-//   if (!match) {
-//     return fail('can not match "!function(e,t)"')
-//   }
-//   const g = match[1]
-//   const f = match[2]
-//   if (!g || !f) {
-//     return fail('can not match "g" and "f"', { g, f })
-//   }
-//   // e.amd?define(e):t.VueRouter=e()}(t
-//   const match2 = jsText.match(new RegExp(`:${g}.(\\w+)=${f}\\(\\)`))
-//   if (!match2) {
-//     return fail('can not match "e.amd?define(e):t.VueRouter=e()}(t"')
-//   }
-//   const v = match2[1]
-//   if (!v) {
-//     return fail('can not match "v"')
-//   }
-//   return {
-//     result: [v]
-//   }
-// }
 
 @Service()
 @JsonController('/requests/get')
 export class RequestsGetController {
   @Get('/page')
-  async getPageInfo (@QueryParams() query: PageQuery) {
-    const { data: htmlText } = await Axios.get(query.url)
-    const $ = Cheerio.load(htmlText)
-    // 获取链接的protocol
+  async getPageInfo (@QueryParam('url') queryUrl: string) {
     // 自动补全链接
     // 获取js文件中的全局变量
+    const url = new URL(queryUrl)
+    const { data: htmlText } = await Axios.get(url.toString())
+    const $ = Cheerio.load(htmlText)
+    const mapUrl = (attr, options = { vars: false }) => (_, el) => {
+      const formattedUrl = new URL($(el).attr(attr), url).toString()
+      if (options.vars) {
+        return getGlobalVars(formattedUrl).then(vars => ({
+          url: formattedUrl,
+          vars
+        }))
+      }
+      return formattedUrl
+    }
     return {
-      scripts: $('script[src]').map((_, el) => $(el).attr('src')).get(),
-      prefetch: $('link[rel=prefetch]').map((_, el) => $(el).attr('href')).get(),
-      preload: $('link[rel=preload]').map((_, el) => $(el).attr('href')).get(),
-      styles: $('link[rel=stylesheet]').map((_, el) => $(el).attr('href')).get()
+      scripts: await Promise.all($('script[src]').map(mapUrl('src', { vars: true })).get()),
+      prefetch: $('link[rel=prefetch]').map(mapUrl('href')).get(),
+      preload: $('link[rel=preload]').map(mapUrl('href')).get(),
+      styles: $('link[rel=stylesheet]').map(mapUrl('href')).get()
     }
   }
 }
